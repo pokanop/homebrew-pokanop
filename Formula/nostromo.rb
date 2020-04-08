0a17a2eda5878b5295877fd0b569911d9e309cbb
saheljalal <sahel.jalal@icloud.com>
2020-04-07 20:44:26 -0700

    Brew formula update for nostromo version v0.7.2

--- Formula/nostromo.rb
+++ Formula/nostromo.rb
@@ -2,16 +2,16 @@
 class Nostromo < Formula
   desc "nostromo is a CLI to manage aliases through simple commands to add and remove scoped aliases and substitutions."
   homepage "https://nostromo.sh"
-  version "0.7.1"
+  version "0.7.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pokanop/nostromo/releases/download/v0.7.1/nostromo_0.7.1_Darwin_x86_64.tar.gz"
-    sha256 "ebd8bf0cd1247bce4972809d475d757e5205185bc52a7a201e6ddba3d290c8c4"
+    url "https://github.com/pokanop/nostromo/releases/download/v0.7.2/nostromo_0.7.2_Darwin_x86_64.tar.gz"
+    sha256 "59e0c0d0f6a1a122a724ab5eb1279bb5ec2ba8c5d76baaef2d5f2b7fa26d29fe"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/pokanop/nostromo/releases/download/v0.7.1/nostromo_0.7.1_Linux_x86_64.tar.gz"
-      sha256 "88b6ce7ba6ece119eda1cbec91dacd2448f82fb6843a79e45ad488f47f6a535d"
+      url "https://github.com/pokanop/nostromo/releases/download/v0.7.2/nostromo_0.7.2_Linux_x86_64.tar.gz"
+      sha256 "7d8d3256c1634343033e3f68ae9a8435fc0fca08e1a097cf13b4c35a9f9ae7df"
     end
   end
 
